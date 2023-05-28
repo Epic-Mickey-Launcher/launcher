c6c7ee05a7c484c0afddf20b9a9a56584207ecb6
Kjøb <57041363+KjubDusJub@users.noreply.github.com>
2023-05-28 18:39:10 +0200

    Vote System & Download Counter

--- EpicMickeyLauncher/src/routes/modpage.svelte
+++ EpicMickeyLauncher/src/routes/modpage.svelte
@@ -13,6 +13,9 @@
     import { invoke } from "@tauri-apps/api/tauri";
     import { Subscribe } from "./library/callback";
 
+    let downloads = 0;
+    let likes = 0;
+
     let authoraccountexists = true;
     let modid;
     let authorname = "";
@@ -25,6 +28,13 @@
         modid = GetData("modpage_id");
         dumploc = GetData("modpage_dumploc");
         modinfo = await POST("getmod", { id: modid });
+        let token = await GetToken();
+        let impressions = await POST("getmodimpressions", { token: token, mod: modid });
+        downloads = impressions.downloads;
+        likes = impressions.likes;
+        if(impressions.liked){
+            hearticon.style.fill = "red";
+        }
         let userinfo = await POST("getaccount", { id: modinfo.author });
         if (userinfo.username == null) {
             authorname = "Unknown Account";
@@ -72,6 +82,21 @@
         SetData("modupload_id", modinfo.id);
         window.open("#/uploadmod", "_self");
     }
+
+    async function LikeMod(){
+        let token = await GetToken();
+        let response = await POST("addmodimpression", {token: token, modid: modinfo.id, impression:{download:false, like:true}}) 
+        console.log(response)
+        likes += response.likes;
+        if(likes > 0){
+            hearticon.style.fill = "red";
+        }
+        else{
+            hearticon.style.fill = "white";
+        }
+    }
+
+    let hearticon;
 
     async function Download() {
         let gameid;
@@ -121,6 +146,8 @@
             );
             modInstallElement.$destroy();
             CheckIfDownloaded();
+            let token = await GetToken();
+            await POST("addmodimpression", {token: token, modid: modinfo.id, impression:{download:true, like:false}}) 
         });
     }
     let update = false;
@@ -161,7 +188,8 @@
             style="border-radius:20px;margin-right:20px;width:200px;height:200px;"
         />
         <div>
-            <span style="font-size:30px;">{modinfo.name}</span> 
+            <span style="font-size:30px;">{modinfo.name}</span> <svg style="width:25px;height:25px;margin-left:10px;fill:white;"><path d="M12.033,19.011a3.489,3.489,0,0,0,2.475-1.024l3.919-3.919-2.121-2.121-2.782,2.782L13.5,0l-3,0,.024,14.709L7.76,11.947,5.639,14.068l3.919,3.919A3.487,3.487,0,0,0,12.033,19.011Z"/><path d="M21,16v5H3V16H0v5a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V16Z"/></svg> <span style="margin-left: 3px;">{downloads}</span>
+            <button on:click={LikeMod} style="background:transparent;border:none;width:25px;height:25px;margin-left:10px;"><svg bind:this={hearticon} width="25px" height="25px" style="fill:white;"><path d="M12,23.462l-.866-.612C9.994,22.044,0,14.783,0,8.15A7.036,7.036,0,0,1,6.75.875,6.57,6.57,0,0,1,12,3.582,6.57,6.57,0,0,1,17.25.875,7.036,7.036,0,0,1,24,8.15c0,6.633-9.994,13.894-11.134,14.7ZM6.75,3.875A4.043,4.043,0,0,0,3,8.15c0,3.916,5.863,9.21,9,11.611,3.137-2.4,9-7.695,9-11.611a4.043,4.043,0,0,0-3.75-4.275A4.043,4.043,0,0,0,13.5,8.15h-3A4.043,4.043,0,0,0,6.75,3.875Z"/></svg></button><span style="margin-left: 10px;">{likes}</span>
             <p>
                 <button on:click={OpenProfileOfAuthor} class="hyperlinkbutton"
                     >{authorname}</button
